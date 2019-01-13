1bb45f08baf830e537dfb39f11461931e50e221a
joeldudley <joeldudley@outlook.com>
2019-01-13 13:28:58 +0000

    Renaming.

--- src/main/kotlin/ciphers/toyciphers/ECBUnknownKeyUnknownTrailingBytesCipher.kt
+++ src/main/kotlin/ciphers/toyciphers/ECBUnknownKeyUnknownTrailingBytesCipher.kt
@@ -6,13 +6,13 @@ import utilities.base64ToBytes
 
 /**
  * A cipher that encrypts the plaintext with AES in ECB mode with an unknown fixed key after appending unknown fixed
- * trailing bytes.
+ * bytes.
  */
 
-class ECBUnknownKeyUnknownTrailingBytesCipher : Cipher {
+class ECBUnknownKeyUnknownAppendedBytesCipher : Cipher {
     override val key: ByteArray get() = throw IllegalStateException("The key of this cipher is fixed by unknown.")
     private val unknownKey = "FREEZE CONSTRUCT".toByteArray()
-    private val unknownTrailingBytes = ("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmx" +
+    private val unknownAppendedBytes = ("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmx" +
             "vdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdm" +
             "UgYnkK").base64ToBytes()
     private val cipher = AesEcbCipher()
@@ -22,15 +22,15 @@ class ECBUnknownKeyUnknownTrailingBytesCipher : Cipher {
     }
 
     override fun encrypt(plaintext: ByteArray): ByteArray {
-        return cipher.encrypt(plaintext + unknownTrailingBytes)
+        return cipher.encrypt(plaintext + unknownAppendedBytes)
     }
 
     override fun decrypt(ciphertext: ByteArray): ByteArray {
-        val plaintextWithTrailingBytes = cipher.decrypt(ciphertext)
+        val plaintextWithAppendedBytes = cipher.decrypt(ciphertext)
 
         // TODO: Assuming it was correct to pad with null bytes in AES ECB.
-        val firstPaddingByteIndex = plaintextWithTrailingBytes.indexOf(0.toByte())
-        val firstTrailingByteIndex = firstPaddingByteIndex - unknownTrailingBytes.size
-        return plaintextWithTrailingBytes.sliceArray(0 until firstTrailingByteIndex)
+        val firstPaddingByteIndex = plaintextWithAppendedBytes.indexOf(0.toByte())
+        val firstAppendedByteIndex = firstPaddingByteIndex - unknownAppendedBytes.size
+        return plaintextWithAppendedBytes.sliceArray(0 until firstAppendedByteIndex)
     }
 }
